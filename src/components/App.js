101161388e6cbf15d5db8ab1199d057b19e0b38e
david-aihe <doa9200@gmail.com>
2022-01-30 12:26:08 -0500

    Creted React App. Added components folder. Added CSS reset. Added react router.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Plant Water Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
