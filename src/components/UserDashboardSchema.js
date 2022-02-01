e4e4ed0e6f97f4bb0d8790beeef2c735f88784c5
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-02-01 14:22:28 -0800

    User-board-schema

---
+++ src/components/UserDashboardSchema.js
@@ -0,0 +1,22 @@
+import * as yup from 'yup';
+
+const UserDashboardSchema = yup.object().shape({
+    username: yup
+    .string()
+    .trim()
+    .required('Name is required')
+    .min(3, 'Must be at least 3 characters!')
+    .max(30, "you've exceeded the limit on characters"),
+    password: yup
+    .string()
+    .trim()
+    .required("password is required")
+    .min(3, "Password must be longer")
+    .max(30, "password is too long."),
+    phone: yup
+    .string()
+    .required('your # is required')
+    .max(8, 'must be your full phone #')
+})
+
+export default UserDashboardSchema;
